64347fd532f8e4eb160b5e9d8a5053d5470692a5
lucasbarroos <lbarros.comp@gmail.com>
2020-06-01 17:10:40 -0300

    Inserting the animation to scroll top

--- src/pages/Video/index.js
+++ src/pages/Video/index.js
@@ -86,7 +86,7 @@ const Video = ({ user, dispatch }) => {
   };
 
   useEffect(() => {
-    window.scroll(0, 0);
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
     setSpinner(true);
     getVideo();
     getRelatedVideos();
